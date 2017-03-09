640198da8681d9518439bafd9d593545d17c34d4
Óscar García Amor <ogarcia@connectical.com>
2017-03-09 10:24:58 +0100

    Big import

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:3.5
+
+COPY . /tmp/
+
+ADD https://github.com/grafana/grafana/archive/v4.1.2.tar.gz /tmp/build/v4.1.2.tar.gz
+
+RUN /tmp/docker/build.sh
+
+EXPOSE 3000
+
+VOLUME [ "/etc/grafana", "/var/lib/grafana", "/var/log/grafana" ]
+
+USER grafana
+
+ENTRYPOINT [ "/usr/bin/grafana-server", "-homepath", "/usr/share/grafana", "-config", "/etc/grafana/grafana.ini" ]
---
+++ docker/build.sh
@@ -0,0 +1,57 @@
+#! /bin/sh
+#
+# build.bash
+# Copyright (C) 2017 Óscar García Amor <ogarcia@connectical.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+# upgrade
+apk -U --no-progress upgrade
+
+# install build deps
+apk -U --no-progress add g++ git go make musl-dev nodejs python
+
+# extract software
+cd /tmp/build
+tar xzf v*.tar.gz
+
+# build grafana
+export GOPATH="/tmp/build/gopath"
+export GRAFANAPATH="${GOPATH}/src/github.com/grafana/grafana"
+mkdir -p ${GRAFANAPATH%/*}
+ln -fsT /tmp/build/grafana-* ${GRAFANAPATH}
+cd ${GRAFANAPATH}
+patch -p1 <"/tmp/docker/defaults.patch"
+go run build.go setup
+go run build.go build
+npm install -g yarn
+yarn install --pure-lockfile
+npm run build
+
+# install grafana
+install -D -m755 "${GRAFANAPATH}/bin/grafana-cli" \
+  "/usr/bin/grafana-cli"
+install -D -m755 "${GRAFANAPATH}/bin/grafana-server" \
+  "/usr/bin/grafana-server"
+install -D -m644 "${GRAFANAPATH}/conf/defaults.ini" \
+  "/usr/share/grafana/conf/defaults.ini"
+mv ${GRAFANAPATH}/public_gen /usr/share/grafana/public
+mv ${GRAFANAPATH}/vendor /usr/share/grafana/
+install -D -m644 "${GRAFANAPATH}/conf/sample.ini" \
+  "/etc/grafana/grafana.ini"
+install -D -m644 "${GRAFANAPATH}/conf/ldap.toml" \
+  "/etc/grafana/ldap.toml"
+
+# create grafana user
+addgroup -S grafana
+adduser -S -D -H -h /var/lib/grafana -s /sbin/nologin -G grafana \
+  -g grafana grafana
+mkdir -p /var/lib/grafana
+chown grafana:grafana /var/lib/grafana
+
+# remove build deps
+apk --no-progress del g++ git go make musl-dev nodejs python
+rm -rf /root/.ash_history /root/.cache /root/.config /root/.node-gyp \
+  /root/.npm /root/.yarnrc /tmp/* /usr/bin/yarn* /usr/lib/go \
+  /usr/lib/node_modules /var/cache/apk/*
